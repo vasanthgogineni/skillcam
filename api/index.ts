acd301f00da38f34a5f1c813158379fc736e5087
Vasanth Gogineni <vgogineni2005@gmail.com>
2025-11-25 21:32:24 -0500

    test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import serverless from "serverless-http";
-import { registerRoutes } from "../server/routes";
+import { registerRoutes } from "./routes";
 
 const app = express();
 
