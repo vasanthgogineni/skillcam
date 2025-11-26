697d26f1de519b406792f40361f0eca94f01658a
Vasanth Gogineni <vgogineni2005@gmail.com>
2025-11-25 21:28:24 -0500

    test

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import serverless from "serverless-http";
-import { registerRoutes } from "../server/routes.js";
+import { registerRoutes } from "../server/routes";
 
 const app = express();
 
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes.js";
+import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { testConnection } from "./db";
 
